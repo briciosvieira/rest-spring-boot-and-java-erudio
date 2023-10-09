d091986ea128814c062f77b05307669c340c8fb1
fabricio sousa <fabricio_9327@hotmail.com>
2023-10-09 16:50:23 -0300

    Adição de Migration

---
+++ 01-java/API-Spring-Boot/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+
+-- Copiando dados para a tabela java_erudio.person: ~0 rows (aproximadamente)
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Camacari', 'Caio', 'Male', 'Vieira');
